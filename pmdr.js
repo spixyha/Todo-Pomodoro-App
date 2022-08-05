fd59073a47dc249c2f106369ee8598c13952f0ea
Constantin Lisnic <costelisnic98@gmail.com>
2022-08-05 11:13:05 +0100

    grabbed displayWork and displayBreak

--- pmdr.js
+++ pmdr.js
@@ -2,13 +2,13 @@ const timerDisplay = document.getElementById("timerDisplay");
 const startButton = document.getElementById("startButton");
 const stopButton = document.getElementById("stopButton");
 const resetButton = document.getElementById("resetButton");
-const timeButton = document.getElementById("timeButton");
 const setTimeInput = document.getElementById("setTimeValue");
 const title = document.getElementById("title");
-const breakButton = document.getElementById("breakButton");
 const setBreakTimeInput = document.getElementById("setBreakTimeValue");
 const breakTimeForm = document.getElementById("breakTimeForm");
 const workTimeForm = document.getElementById("workTimeForm");
+const displayWork = document.getElementById("displayWork");
+const displayBreak = document.getElementById("displayBreak");
 
 let minutes = 25;
 let seconds = 0;
