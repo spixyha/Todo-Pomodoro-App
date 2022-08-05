6db0e73b93ed4f4b367a90f9ab6fac95341206d0
Constantin Lisnic <costelisnic98@gmail.com>
2022-08-05 13:25:30 +0100

    added another round button and total rounds display

--- pmdr.js
+++ pmdr.js
@@ -9,6 +9,8 @@ const breakTimeForm = document.getElementById("breakTimeForm");
 const workTimeForm = document.getElementById("workTimeForm");
 const displayWork = document.getElementById("displayWork");
 const displayBreak = document.getElementById("displayBreak");
+const anotherRound = document.getElementById("anotherRound");
+const roundsDisplay = document.getElementById("rounds");
 
 let minutes = 25;
 let seconds = 0;
@@ -16,6 +18,8 @@ let breakMinutes = 5;
 let roundsToStop = 2;
 let timer;
 let display;
+let totalRounds = 0;
+let minutesMemory = [];
 
 function decreaseTime() {
     if (roundsToStop !== 0) {
@@ -31,6 +35,7 @@ function decreaseTime() {
                 } else {
                     minutes = 0;
                     seconds = 0;
+                    totalRounds++;
                 }
             }
         }
@@ -49,8 +54,8 @@ function stopTimer() {
 }
 
 function startTimer() {
-    timer = setInterval(decreaseTime, 1000);
-    display = setInterval(updateDisplay, 1000);
+    timer = setInterval(decreaseTime, 10);
+    display = setInterval(updateDisplay, 10);
 }
 
 const updateMinutes = () => minutes.toString().length === 2 ? minutes : `0${minutes}`;
@@ -62,6 +67,7 @@ function updateDisplay() {
     if (roundsToStop === 1) {
         title.innerHTML = `<span style="color:green;">Break Time</span> ${breakMinutes} min`;
     }
+    roundsDisplay.innerText = `Total Rounds: ${totalRounds}`;
 }
 
 startButton.addEventListener("click", () => {
@@ -69,6 +75,7 @@ startButton.addEventListener("click", () => {
         startTimer();
         startButton.disabled = true;
         stopButton.disabled = false;
+        anotherRound.disabled = false;
     }
 })
 stopButton.addEventListener("click", () => {
@@ -84,18 +91,31 @@ resetButton.addEventListener("click", () => {
     minutes = 25;
     breakMinutes = 5;
     seconds = 0;
+    totalRounds = 0;
+    minutesMemory = [];
     updateDisplay();
     title.innerText = "Pomodoro Timer"
     startButton.disabled = false;
     stopButton.disabled = false;
     displayWork.innerText = "";
     displayBreak.innerText = "";
+    anotherRound.disabled = true;
+})
+
+anotherRound.addEventListener("click", () => {
+    if (totalRounds !== 0) {
+        minutes = minutesMemory[0];
+        breakMinutes = minutesMemory[1];
+        title.innerText = "Pomodoro Timer"
+        startTimer();
+    }
 })
 
 workTimeForm.addEventListener("submit", e => {
     e.preventDefault();
 
     minutes = setTimeInput.value;
+    minutesMemory[0] = minutes;
     updateDisplay();
     displayWork.innerText = `Working Time (${updateMinutes()}:${updateSeconds()})`;
     setTimeInput.value = "";
@@ -104,8 +124,8 @@ workTimeForm.addEventListener("submit", e => {
 breakTimeForm.addEventListener("submit", e => {
     e.preventDefault();
 
-    const input = setBreakTimeInput.value;
-    breakMinutes = Number(input);
+    breakMinutes = setBreakTimeInput.value;
+    minutesMemory[1] = breakMinutes;
     displayBreak.innerText = `Break Time (${breakMinutes}:00)`;
     setBreakTimeInput.value = "";
 })
